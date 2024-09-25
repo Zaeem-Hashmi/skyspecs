66ff604ebd0561ab19576fa4234b03666cbcce9d
Zaeem-Hashmi <syedzae123@gmail.com>
2024-09-26 00:26:17 +0500

    first commit.

---
+++ resources/views/client/index.blade.php
@@ -0,0 +1,181 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Skyspecs Media Company | Home</title>
+
+    {{-- bootstrap links --}}
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    {{-- Custom CSS --}}
+    <link rel="stylesheet" href="{{ asset("client/css/style.css") }}">
+    {{-- fonts --}}
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Courier+Prime:ital,wght@0,400;0,700;1,400;1,700&display=swap" rel="stylesheet">
+</head>
+<body class="bg-beige" style="height: 100vh;">
+    <div class="main-bg position animation" style="opacity: 0.7;z-index:-1;">
+    </div>
+    <img src="{{ asset("client/assets/robot.png") }}" alt="" style="position: fixed;bottom:0;right:0;opacity:0.7;">
+    <section class="px-3" style="z-index: 1;">
+        <div style="height: 50vh">
+            <div class="mt-3">
+                <div>
+                    <img src="{{ asset("client/assets/logo2.svg") }}" alt="">
+                    <h5 class="ps-3 " style="line-height: 20px; text-transform: uppercase;font-weight:800">
+                        Independent <br> minority-owned
+                    </h5>
+                </div>
+            </div>
+            <div class="d-flex justify-content-center">
+                <img src="{{ asset("client/assets/logo.png") }}" style="width:100px" alt="">
+            </div>
+            <div class="d-flex justify-content-center align-items-center mt-3">
+                <a target="_blank" href="https://www.youtube.com/@skyspecsmediaco"><img src="{{ asset("client/assets/youtube.png") }}" alt="" style="width: 50px;height:40px" class="mx-2"></a>
+                <a target="_blank" href="{{ url("/compatibility-statement.pdf") }}"><img src="{{ asset("client/assets/compatibility.png") }}" alt="" style="width: 30px;height:40px" class="mx-2"></a>
+               <a target="_blank" href="https://tiktok.com/@skyspecsmediaco?_t=8jLONH2hVYE&_r=1"><img src="{{ asset("client/assets/tiktok.png") }}" alt="" style="width: 30px;height:40px" class="mx-2"></a>
+                <a target="_blank" href="mailto:skyspecsproductions@gmail.com"><img src="{{ asset("client/assets/mail.png") }}" alt="" style="width: 40px;height:35px" class="mx-2"></a>
+                <a target="_blank" href="http://calendly.com/skyspecsproductions@gmail.com"><img src="{{ asset("client/assets/calendar.png") }}" alt="" style="width: 40px;height:40px" class="mx-2"></a>
+                <img src="{{ asset("client/assets/blog.png") }}" alt="" style="width: 40px;height:40px" class="mx-2">
+                <img src="{{ asset("client/assets/media.png") }}" alt="" style="width: 40px;height:40px" class="mx-2">
+            </div>
+        </div>
+        <div class="mt-5 container-fluid courier-prime-regular" style="overflow-y:scroll;height:50vh;">
+            <div class="row my-3">
+                <div class="col-md-2">
+                    24-3-24
+                </div>
+                <div class="col-md-9">
+                    <div class="accordion" id="accordionExample">
+                        <div class="accordion-item bg-transparent border-0">
+                            <h2 class="accordion-header border-0" id="headingOne">
+                                <button class="accordion-button p-0 bg-transparent text-dark fw-bold"
+                                onclick="typing(`It is shown by default, until the collapse plugin adds the appropriate classes that we use to style each element. These classes control the overall appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. It's also worth noting that just about any HTML can go within the though the transition does limit overflow.`,'one')"
+                                style="box-shadow: 0px 0px 0px black" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+                                    LA COUNTY METROPOLITAN TRANSPORTATION AUTHORITY CERTIFIES SKYSPECS MEDIA COMPANY AS A DBE AND SBE ENTERPRISE
+                                </button>
+                            </h2>
+                            <div id="collapseOne" class="accordion-collapse collapse hide" aria-labelledby="headingOne" data-bs-parent="#accordionExample">
+                                <div class="accordion-body p-0 pt-2" id="one">
+
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="row my-3">
+                <div class="col-md-2">
+                    26-3-24
+                </div>
+                <div class="col-md-9">
+                    <div class="accordion" id="accordionExample">
+                        <div class="accordion-item bg-transparent border-0">
+                            <h2 class="accordion-header border-0" id="heading2">
+                                <button class="accordion-button p-0 bg-transparent text-dark fw-bold" style="box-shadow: 0px 0px 0px black" type="button" data-bs-toggle="collapse" data-bs-target="#collapse2" aria-expanded="true" aria-controls="collapse2">
+                                    SKYSPECS MEDIA COMPANY BECOMES AN MBE CERTIFIED COMPANY THROUGH NMSDC
+                                </button>
+                            </h2>
+                            <div id="collapse2" class="accordion-collapse collapse hide" aria-labelledby="headingOne" data-bs-parent="#accordionExample">
+                                <div class="accordion-body p-0 pt-2">
+                                    ..
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="row my-3">
+                <div class="col-md-2">
+                    26-3-24
+                </div>
+                <div class="col-md-9">
+                    <div class="accordion" id="accordionExample">
+                        <div class="accordion-item bg-transparent border-0">
+                            <h2 class="accordion-header border-0" id="heading3">
+                                <button class="accordion-button p-0 bg-transparent text-dark fw-bold" style="box-shadow: 0px 0px 0px black" type="button" data-bs-toggle="collapse" data-bs-target="#collapse3" aria-expanded="true" aria-controls="collapse3">
+                                    SKYSPECS MEDIA COMPANY BECOMES A MBE CERTIFIED COMPANY THROUGH SUPPLIER CLEARINGHOUSE
+                                </button>
+                            </h2>
+                            <div id="collapse3" class="accordion-collapse collapse hide" aria-labelledby="headingOne" data-bs-parent="#accordionExample">
+                                <div class="accordion-body p-0 pt-2">
+                                    ..
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="row my-3">
+                <div class="col-md-2">
+                    31-1-24
+                </div>
+                <div class="col-md-9">
+                    <div class="accordion" id="accordionExample">
+                        <div class="accordion-item bg-transparent border-0">
+                            <h2 class="accordion-header border-0" id="heading4">
+                                <button class="accordion-button p-0 bg-transparent text-dark fw-bold" style="box-shadow: 0px 0px 0px black" type="button" data-bs-toggle="collapse" data-bs-target="#collapse4" aria-expanded="true" aria-controls="collapse4">
+                                    SKYSPECS MEDIA COMPANY BECOMES AN EBE, SBE(PROPRIETARY), SBE (CITY OF LOS ANGELES), VSBE(HARBOR) CERTIFIED COMPANY
+                                </button>
+                            </h2>
+                            <div id="collapse4" class="accordion-collapse collapse hide" aria-labelledby="headingOne" data-bs-parent="#accordionExample">
+                                <div class="accordion-body p-0 pt-2">
+                                    ..
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="row my-3">
+                <div class="col-md-2">
+                    22-12-23
+                </div>
+                <div class="col-md-9">
+                    <div class="accordion" id="accordionExample">
+                        <div class="accordion-item bg-transparent border-0">
+                            <h2 class="accordion-header border-0" id="heading5">
+                                <button class="accordion-button p-0 bg-transparent text-dark fw-bold" style="box-shadow: 0px 0px 0px black" type="button" data-bs-toggle="collapse" data-bs-target="#collapse5" aria-expanded="true" aria-controls="collapse5">
+                                    THE FORMATION OF SKYSPECS MEDIA COMPANY
+                                </button>
+                            </h2>
+                            <div id="collapse5" class="accordion-collapse collapse hide" aria-labelledby="heading5" data-bs-parent="#accordionExample">
+                                <div class="accordion-body p-0 pt-2">
+                                    ..
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    </section>
+    {{-- Bootstrap JS --}}
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+    <script>
+        let i,txt,id,speed=10;
+        function typing(text,section) {
+        i = 0;
+        txt = text;
+        id = section;
+        document.getElementById(id).innerHTML = "";
+        print();
+    }
+
+    function print()
+        {
+            if (i < txt.length) {
+                console.log(id);
+
+            document.getElementById(id).innerHTML += txt.charAt(i);
+            i++;
+            setTimeout(print, speed);
+          }
+    }
+
+    </script>
+</body>
+</html>
