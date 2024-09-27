51fe2f24acafa65d1fce28de3c3c7c77efbb334e
Zaeem-Hashmi <syedzae123@gmail.com>
2024-09-27 10:29:00 +0500

    blogs section started

---
+++ resources/views/client/blog/modal.blade.php
@@ -0,0 +1,59 @@
+  <!-- Modal -->
+  <div class="modal fade" id="blog" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+      <div class="modal-dialog">
+          <div class="modal-content">
+              <div class="modal-header justify-content-center border-0">
+                  <div>
+                      <img src="{{ asset("client/assets/logo.png") }}" alt="" width="80px;" class="d-block mx-auto">
+                      <h2 class="modal-title text-center fw-bold mt-3" id="exampleModalLabel" style="line-height: 20px">BLOGS AND NEWS</h2>
+                  </div>
+                  <button type="button" class="btn-close rounded-circle" data-bs-dismiss="modal" aria-label="Close" style="position: absolute;top:20px;right:20px"></button>
+              </div>
+              <div class="modal-body bg-cream">
+                <h3 class="modal-title text-center mt-3 mt-3 bg-light-cream mx-auto px-3 py-3" id="exampleModalLabel" style="line-height: 20px;width:320px;min-width:280px;font-weight:900;">POPULAR TOPICS</h3>
+                <div class="owl-carousel owl-theme px-3 mt-3">
+                    <div class="bg-light p-3">
+                        <img src="{{ asset("client/assets/blog/blog1.png") }}" width="100%" alt="" class="shadow">
+                        <p class="fw-bold text-center text-uppercase" style="font-size: 13px">
+                            The Future of Filmmaking: How AI and Machine Learning Are Revolutionizing Video Production
+                        </p>
+                        <p class="" style="font-size: 12px;text-align:center;">
+                            Artificial Intelligence (AI) and Machine Learning (ML) are not just transforming industries—they are revolutionizing the world of filmmaking and video production.
+                        </p>
+                    </div>
+                    <div class="bg-light p-3">
+                        <img src="{{ asset("client/assets/blog/blog2.png") }}" width="100%" alt="" class="shadow">
+                        <p class="fw-bold text-center text-uppercase mt-3" style="font-size: 13px">
+                            Going Live: The Explosive Rise of Live Streaming and Its Impact on Media
+                        </p>
+                        <p class="" style="font-size: 12px;text-align:center;">
+                            live streaming trends, Kai Cenat live streams, iShowSpeed popularity
+                        </p>
+                    </div>
+                    <div class="bg-light p-3">
+                        <img src="{{ asset("client/assets/blog/blog3.png") }}" width="100%" alt="" class="shadow">
+                        <p class="fw-bold text-center text-uppercase mt-3" style="font-size: 13px">
+                            Virtual Reality (VR) and Augmented Reality (AR): Immersive Experiences in Video Production
+                        </p>
+                        <p class="" style="font-size: 12px;text-align:center;">
+                            VR in video production, AR technology, immersive video experiences
+                        </p>
+                    </div>
+                    <div class="bg-light p-3">
+                        <img src="{{ asset("client/assets/blog/blog4.png") }}" width="100%" alt="" class="shadow">
+                        <p class="fw-bold text-center text-uppercase mt-3" style="font-size: 13px">
+                            Capturing Every Angle: The Rise of 360-Degree Videos
+                        </p>
+                        <p class="" style="font-size: 12px;text-align:center;">
+                            360-degree video production, immersive video content, VR videos
+                        </p>
+                    </div>
+                  </div>
+              </div>
+              <div class="modal-body">
+                ...
+            </div>
+          </div>
+      </div>
+  </div>
+
--- resources/views/client/index.blade.php
+++ resources/views/client/index.blade.php
@@ -14,8 +14,12 @@
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Courier+Prime:ital,wght@0,400;0,700;1,400;1,700&display=swap" rel="stylesheet">
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.theme.default.css" rel="stylesheet"/>
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.carousel.min.css" rel="stylesheet"/>
 </head>
 <body class="bg-beige" style="height: 100vh;">
+    @include('client.blog.modal')
+    @include('client.media.modal')
     <div class="main-bg position animation" style="opacity: 0.7;z-index:-1;">
     </div>
     <img src="{{ asset("client/assets/robot.png") }}" alt="" style="position: fixed;bottom:0;right:0;opacity:0.7;">
@@ -35,11 +39,11 @@
             <div class="d-flex justify-content-center align-items-center mt-3">
                 <a target="_blank" href="https://www.youtube.com/@skyspecsmediaco"><img src="{{ asset("client/assets/youtube.png") }}" alt="" style="width: 50px;height:40px" class="mx-2"></a>
                 <a target="_blank" href="{{ url("/compatibility-statement.pdf") }}"><img src="{{ asset("client/assets/compatibility.png") }}" alt="" style="width: 30px;height:40px" class="mx-2"></a>
-               <a target="_blank" href="https://tiktok.com/@skyspecsmediaco?_t=8jLONH2hVYE&_r=1"><img src="{{ asset("client/assets/tiktok.png") }}" alt="" style="width: 30px;height:40px" class="mx-2"></a>
+                <a target="_blank" href="https://tiktok.com/@skyspecsmediaco?_t=8jLONH2hVYE&_r=1"><img src="{{ asset("client/assets/tiktok.png") }}" alt="" style="width: 30px;height:40px" class="mx-2"></a>
                 <a target="_blank" href="mailto:skyspecsproductions@gmail.com"><img src="{{ asset("client/assets/mail.png") }}" alt="" style="width: 40px;height:35px" class="mx-2"></a>
                 <a target="_blank" href="http://calendly.com/skyspecsproductions@gmail.com"><img src="{{ asset("client/assets/calendar.png") }}" alt="" style="width: 40px;height:40px" class="mx-2"></a>
-                <img src="{{ asset("client/assets/blog.png") }}" alt="" style="width: 40px;height:40px" class="mx-2">
-                <img src="{{ asset("client/assets/media.png") }}" alt="" style="width: 40px;height:40px" class="mx-2">
+                <img data-bs-toggle="modal" data-bs-target="#blog" src="{{ asset("client/assets/blog.png") }}" alt="" style="width: 40px;height:40px" class="mx-2">
+                <img data-bs-toggle="modal" data-bs-target="#media" src="{{ asset("client/assets/media.png") }}" alt="" style="width: 40px;height:40px" class="mx-2">
             </div>
         </div>
         <div class="mt-5 container-fluid courier-prime-regular" style="overflow-y:scroll;height:50vh;">
@@ -51,9 +55,7 @@
                     <div class="accordion" id="accordionExample">
                         <div class="accordion-item bg-transparent border-0">
                             <h2 class="accordion-header border-0" id="headingOne">
-                                <button class="accordion-button p-0 bg-transparent text-dark fw-bold"
-                                onclick="typing(`It is shown by default, until the collapse plugin adds the appropriate classes that we use to style each element. These classes control the overall appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. It's also worth noting that just about any HTML can go within the though the transition does limit overflow.`,'one')"
-                                style="box-shadow: 0px 0px 0px black" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+                                <button class="accordion-button p-0 bg-transparent text-dark fw-bold" onclick="typing(`It is shown by default, until the collapse plugin adds the appropriate classes that we use to style each element. These classes control the overall appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. It's also worth noting that just about any HTML can go within the though the transition does limit overflow.`,'one')" style="box-shadow: 0px 0px 0px black" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
                                     LA COUNTY METROPOLITAN TRANSPORTATION AUTHORITY CERTIFIES SKYSPECS MEDIA COMPANY AS A DBE AND SBE ENTERPRISE
                                 </button>
                             </h2>
@@ -155,27 +157,57 @@
     </section>
     {{-- Bootstrap JS --}}
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+    <script src="https://code.jquery.com/jquery-3.7.1.min.js" integrity="sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo=" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/owl.carousel.min.js"></script>
     <script>
-        let i,txt,id,speed=10;
-        function typing(text,section) {
-        i = 0;
-        txt = text;
-        id = section;
-        document.getElementById(id).innerHTML = "";
-        print();
-    }
+        let i, txt, id, speed = 10;
 
-    function print()
-        {
+        function typing(text, section) {
+            i = 0;
+            txt = text;
+            id = section;
+            document.getElementById(id).innerHTML = "";
+            print();
+        }
+
+        function print() {
             if (i < txt.length) {
                 console.log(id);
 
-            document.getElementById(id).innerHTML += txt.charAt(i);
-            i++;
-            setTimeout(print, speed);
-          }
-    }
+                document.getElementById(id).innerHTML += txt.charAt(i);
+                i++;
+                setTimeout(print, speed);
+            }
+        }
 
     </script>
+    <script>
+        $('.owl-carousel').owlCarousel({
+            loop: true,
+            items: 1,
+            margin:20,
+            autoplay: true,
+            autoplayTimeout: 2000,
+            autoplayHoverPause: true,
+            dots: true,
+            responsive: {
+                0: {
+                    items: 1
+                    , nav: true
+                }
+                , 600: {
+                    items: 2
+                    , nav: false
+                }
+                , 1000: {
+                    items: 3
+                    , nav: true
+                    , loop: false
+                }
+            }
+        })
+
+    </script>
+
 </body>
 </html>
